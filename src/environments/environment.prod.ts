ce25317914df840009407d0d3bcbfe283e1d6dc8
tmushayahama <tmushayahama@gmail.com>
2022-03-24 09:41:30 -0700

    Build production for testing bioghost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -46,7 +46,7 @@ const globalKnownRelations = typeof global_known_relations !== 'undefined'
 export const environment = {
   production: true,
   isDev: false,
-  isBeta: false,
+  isBeta: true,
   isGraph: false,
   spaqrlApiUrl: 'http://rdf-internal.berkeleybop.io/blazegraph/sparql',
   globalGolrNeoServer: golrNeoServer,
